896502d25cc149442aa9ffd22cf53b0d808e3f7b
muditmehta07 <muditmehta27@outlook.com>
2026-06-26 01:37:34 +0530

    new

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+
+# Allow local container connections to your Fedora GUI display
+xhost +local:docker
+
 docker run -it \
   --name ros2_container \
+  --rm \
   --privileged \
   --network host \
   --ipc host \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  -v ~/Talos:/home/talos/ros2_ws \
+  -v /home/mudit/Projects/Talos:/home/talos/ros2_ws \
   ros2-humble-image
+
+# Revoke permission when the container closes (for security)
+xhost -local:docker
